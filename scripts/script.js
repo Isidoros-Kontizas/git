a981af80378ea9420056b65d41486b8bb9484140
Isidoros  Kontizas <13-13940@saeinstitute.edu>
2024-12-11 20:31:33 +0200

    My first commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+const arr = [1, 2, 4, 6, 10, 20];
+
+const arr2 = arr.slice(0, 4); //first number is the begining, second number is the end but the value is NOT included
+
+console.log("This is arr:", arr);
+console.log("This is arr:", arr2);
+
+
+const arrSplice = [1, 2, 4, 6, 10, 20];
+
+const arrSplice2 = arr.slice(0, 4);
